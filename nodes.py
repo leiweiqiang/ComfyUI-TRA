1f3398d8ff0a5b29252aff34768e7521968a14b6
UguurDemir <ugurdemir90@gmail.com>
2024-07-03 16:54:01 -0700

    Implemented YOLOv8 node

--- nodes.py
+++ nodes.py
@@ -1,14 +1,15 @@
 from .tcl_ebsynth_node import TclEbSynth
 from .tcl_extract_video_frames_node import TclExtractFramesFromVideoFile, TclExtractFramesFromVideo
 from .tcl_frames2video_node import TclFrames2Video, TclSaveVideoFromFrames
-
+from .tcl_yolov8 import TclYoloV8Segmentation
 
 NODE_CLASS_MAPPINGS = {
     "TclEbSynth": TclEbSynth,
     "TclExtractFramesFromVideoFile": TclExtractFramesFromVideoFile,
     "TclExtractFramesFromVideo": TclExtractFramesFromVideo,
     "TclFrames2Video": TclFrames2Video,
-    "TclSaveVideoFromFrames": TclSaveVideoFromFrames
+    "TclSaveVideoFromFrames": TclSaveVideoFromFrames,
+    "TclYoloV8Segmentation": TclYoloV8Segmentation
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
@@ -17,5 +18,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "TclExtractFramesFromVideoFile": "TCL Extract Frames (From File)",
     "TclExtractFramesFromVideo": "TCL Extract Frames (From Video)",
     "TclFrames2Video": "TCL Combine Frames",
-    "TclSaveVideoFromFrames": "TCL Save Video (From Frames)"
+    "TclSaveVideoFromFrames": "TCL Save Video (From Frames)",
+    "TclYoloV8Segmentation": "TCL YoloV8 Segmentation"
 }
