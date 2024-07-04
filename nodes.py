caf50046cf427609ad21258f117d1d465cf6aec2
UguurDemir <ugurdemir90@gmail.com>
2024-07-03 17:08:24 -0700

    Implemented Yolov9 node

--- nodes.py
+++ nodes.py
@@ -2,6 +2,7 @@ from .tcl_ebsynth_node import TclEbSynth
 from .tcl_extract_video_frames_node import TclExtractFramesFromVideoFile, TclExtractFramesFromVideo
 from .tcl_frames2video_node import TclFrames2Video, TclSaveVideoFromFrames
 from .tcl_yolov8 import TclYoloV8Segmentation
+from .tcl_yolov9 import TclYoloV9Segmentation
 
 NODE_CLASS_MAPPINGS = {
     "TclEbSynth": TclEbSynth,
@@ -9,7 +10,8 @@ NODE_CLASS_MAPPINGS = {
     "TclExtractFramesFromVideo": TclExtractFramesFromVideo,
     "TclFrames2Video": TclFrames2Video,
     "TclSaveVideoFromFrames": TclSaveVideoFromFrames,
-    "TclYoloV8Segmentation": TclYoloV8Segmentation
+    "TclYoloV8Segmentation": TclYoloV8Segmentation,
+    "TclYoloV9Segmentation":TclYoloV9Segmentation
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
@@ -19,5 +21,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "TclExtractFramesFromVideo": "TCL Extract Frames (From Video)",
     "TclFrames2Video": "TCL Combine Frames",
     "TclSaveVideoFromFrames": "TCL Save Video (From Frames)",
-    "TclYoloV8Segmentation": "TCL YoloV8 Segmentation"
+    "TclYoloV8Segmentation": "TCL YoloV8 Segmentation",
+    "TclYoloV9Segmentation": "TCL YoloV9 Segmentation",
 }
