eb1fa604077b06c9aa643af9163e36326c65e6d7
weiqiang lei <weiqiang.lei@gmail.com>
2024-08-25 21:07:51 -0700

    no message

--- nodes.py
+++ nodes.py
@@ -6,6 +6,7 @@ from .tcl_yolov8 import TclYoloV8Segmentation
 from .tcl_yolov9 import TclYoloV9Segmentation
 from .tcl_lora_save_images import TclLoraSaveImages
 from .tcl_lora_gen_datasets import TclLoraGenDatasets
+from .tcl_lora_training import TclLoraTraining
 
 NODE_CLASS_MAPPINGS = {
     "TclEbSynth": TclEbSynth,
@@ -17,7 +18,8 @@ NODE_CLASS_MAPPINGS = {
     "TclYoloV8Segmentation": TclYoloV8Segmentation,
     "TclYoloV9Segmentation":TclYoloV9Segmentation,
     "TclLoraSaveImages":TclLoraSaveImages,
-    "TclLoraGenDatasets":TclLoraGenDatasets
+    "TclLoraGenDatasets":TclLoraGenDatasets,
+    "TclLoraTraining":TclLoraTraining
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
@@ -31,5 +33,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "TclYoloV8Segmentation": "TCL YoloV8 Segmentation",
     "TclYoloV9Segmentation": "TCL YoloV9 Segmentation",
     "TclLoraSaveImages": "TCL Lora Save Images",
-    "TclLoraGenDatasets":"TCL Lora Gen Datasets"
+    "TclLoraGenDatasets":"TCL Lora Gen Datasets",
+    "TclLoraTraining":"TCL Lora Training"
 }
