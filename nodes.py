2e77dce79e7e9d3b8defeb3fa086d7e62879d981
weiqiang lei <weiqiang.lei@gmail.com>
2024-09-12 08:56:58 -0700

    no message

--- nodes.py
+++ nodes.py
@@ -1,44 +1,10 @@
-from .tcl_ebsynth_node import TclEbSynth
-from .tcl_ebsynth_batch import TclEbSynthBatch
-from .tcl_extract_video_frames_node import TclExtractFramesFromVideoFile, TclExtractFramesFromVideo
-from .tcl_frames2video_node import TclFrames2Video, TclSaveVideoFromFrames
-from .tcl_yolov8 import TclYoloV8Segmentation
-from .tcl_yolov9 import TclYoloV9Segmentation
-from .tcl_lora_save_images import TclLoraSaveImages
-from .tcl_lora_gen_datasets import TclLoraGenDatasets
-from .tcl_lora_training import TclLoraTraining
 from .tcl_fresco import TclFresco
-from .tcl_exr2png import TclExr2png
 
 NODE_CLASS_MAPPINGS = {
-    "TclEbSynth": TclEbSynth,
-    "TclEbSynthBatch": TclEbSynthBatch,
-    "TclExtractFramesFromVideoFile": TclExtractFramesFromVideoFile,
-    "TclExtractFramesFromVideo": TclExtractFramesFromVideo,
-    "TclFrames2Video": TclFrames2Video,
-    "TclSaveVideoFromFrames": TclSaveVideoFromFrames,
-    "TclYoloV8Segmentation": TclYoloV8Segmentation,
-    "TclYoloV9Segmentation":TclYoloV9Segmentation,
-    "TclLoraSaveImages":TclLoraSaveImages,
-    "TclLoraGenDatasets":TclLoraGenDatasets,
-    "TclLoraTraining":TclLoraTraining,
     "TclFresco":TclFresco,
-    "TclExr2png":TclExr2png
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "TclEbSynth": "TCL EbSynth",
-    "TclEbSynthBatch": "TCL EbSynth (Batch)",
-    "TclExtractFramesFromVideoFile": "TCL Extract Frames (From File)",
-    "TclExtractFramesFromVideo": "TCL Extract Frames (From Video)",
-    "TclFrames2Video": "TCL Combine Frames",
-    "TclSaveVideoFromFrames": "TCL Save Video (From Frames)",
-    "TclYoloV8Segmentation": "TCL YoloV8 Segmentation",
-    "TclYoloV9Segmentation": "TCL YoloV9 Segmentation",
-    "TclLoraSaveImages": "TCL Lora Save Images",
-    "TclLoraGenDatasets":"TCL Lora Gen Datasets",
-    "TclLoraTraining":"TCL Lora Training",
     "TclFresco":"Tcl Fresco",
-    "TclExr2png":"Tcl Exr2png"
 }
--- utils.py
+++
@@ -1,8 +0,0 @@
-import os
-import folder_paths
-
-def get_temp_dir():
-    temp_dir = folder_paths.get_temp_directory()
-    if not os.path.exists(temp_dir):
-        os.makedirs(temp_dir)
-    return temp_dir
