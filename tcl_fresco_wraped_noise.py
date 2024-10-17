15f1f038028e9c6faa89acfc5bd2a4367218b0b9
weiqiang lei <weiqiang.lei@gmail.com>
2024-10-17 14:30:01 -0700

    no message

--- tcl_fresco_wraped_noise.py
+++ tcl_fresco_wraped_noise.py
@@ -8,43 +8,6 @@ from server import PromptServer
 import concurrent.futures
 
 class TclFrescoWrapedNoise:
-
-    def __init__(self):
-        pass
-
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            'required': {
-                "model_type": (["XL", "kolors"],),
-                "controlnet_strength": ("FLOAT", {"default": 0.7, "min": 0.7, "max": 1.0}),
-                "ip_adapter_scale": ("FLOAT", {"default": 0.3, "min": 0.3, "max": 0.9}),
-                "max_images": ("INT", {"default": 3, "min": 3, "max": 200}),
-                "prompt": ("STRING", {"multiline": True, "default": ""}),
-                "negative": ("STRING", {"multiline": True, "default": ""}),
-                "video_path": ("STRING", {"multiline": False, "default": "/workspace/FRESCO-wraped-noise/data/example/video_square.mp4"}),
-                "save_path": ("STRING", {"multiline": False, "default": "/workspace/FRESCO-wraped-noise/output888"}),
-                "ref_img_ip_adapter": ("STRING", {"multiline": False, "default": "/workspace/FRESCO-wraped-noise/data/example/ref_img.png"}),
-                "noise_type": ("STRING", {"multiline": False, "default": "integral"}),
-            },
-        }
-
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("help",)
-    FUNCTION = "fresco"
-    OUTPUT_NODE = False
-    CATEGORY = "TCL Research America"
-
-import subprocess
-import yaml
-import os
-import json
-import logging
-import requests
-from server import PromptServer
-import concurrent.futures
-
-class TclFrescoWrapedNoise:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.prompt_server = PromptServer.instance
@@ -56,9 +19,12 @@ class TclFrescoWrapedNoise:
         return {
             'required': {
                 "model_type": (["XL", "kolors"],),
-                "controlnet_strength": ("FLOAT", {"default": 0.7, "min": 0.7, "max": 1.0}),
+                "controlnet_type_xl": (["canny-xl", "depth-xl", "mistoline-xl", "canny+depth-xl", "mistoline+depth-xl"]),
+                "controlnet_type_kolors": (["canny-kolors", "depth-kolors", "canny+depth-kolors"]),
+                "controlnet_strength": ("FLOAT", {"default": 0.5, "min": 0.3, "max": 1.0}),
                 "ip_adapter_scale": ("FLOAT", {"default": 0.3, "min": 0.3, "max": 0.9}),
                 "max_images": ("INT", {"default": 3, "min": 3, "max": 200}),
+                "ip_adapter_style_only": ("STRING", {"multiline": True, "default": ""}),
                 "prompt": ("STRING", {"multiline": True, "default": ""}),
                 "negative": ("STRING", {"multiline": True, "default": ""}),
                 "video_path": ("STRING", {"multiline": False, "default": "/workspace/FRESCO-wraped-noise/data/example/video_square.mp4"}),
@@ -104,7 +70,7 @@ class TclFrescoWrapedNoise:
             self.logger.error(f"Error sending log data: {str(e)}")
 
 
-    def fresco(self, model_type, controlnet_strength, ip_adapter_scale, max_images, prompt, negative, video_path, save_path, ref_img_ip_adapter, noise_type, prompt_id):
+    def fresco(self, model_type, controlnet_type_xl, controlnet_type_kolors, controlnet_strength, ip_adapter_scale, max_images, prompt, negative, video_path, save_path, ref_img_ip_adapter, noise_type, ip_adapter_style_only, prompt_id):
         self.prompt_id = prompt_id
         # Define the base YAML configurations
         yaml_data = {
@@ -113,9 +79,8 @@ class TclFrescoWrapedNoise:
                 "model_type": "kolors",
                 "guidance_scale": 7.5,
                 "use_controlnet": True,
-                "controlnet_path": "canny-kolors",
-                "controlnet_type": "canny-kolors",
-                "controlnet_strength": 1.0,
+                "controlnet_type": "canny+depth-kolors",
+                "controlnet_strength": 0.5,
                 "num_inference_steps": 50,
                 "video_path": "./data/example/video_square.mp4",
                 "save_path": "./output/test/kolors",
@@ -125,45 +90,47 @@ class TclFrescoWrapedNoise:
                 "prompt": "A beautiful photograph of a french street, straight-on 600mm telephoto lens shot f/32",
                 "negative": "(blurry, out of focus, unclear, depth of field, indistinct, 35mm, bokeh, blurred background, blurred foreground, blurred subject, hazy, foggy, washed out, fuzzy, faint)",
                 "batch_size": 3,
-                "ip_adapter_name": "ip_adapter_plus_general.bin",
-                "ip_adapter_scale": 0.8,
-                "ref_img_ip_adapter": "./data/example/ref_img.png",
                 "use_saliency": False,
                 "end_opt_step": 45,
                 "seed": 0,
-                "img_size": 1024,
                 "scheduler": "DDPMScheduler",
+                "ip_adapter_name": "ip_adapter_plus_general.bin",
+                "ip_adapter_scale": 0.8,
+                "ip_adapter_style_only": True,
+                "ref_img_ip_adapter": "./data/example/ref_img.png",
+                "strength": 1.0,
                 "intra_weight": 0.0,
-                "strength": 0.6,
-                "noise_type": "integral"
+                "noise_type": "integral",
+                "offload_to_cpu": True
             },
             "XL": {
                 "sd_path": "./model/checkpoints/juggernautXL_v9Rundiffusionphoto2.safetensors",
                 "model_type": "XL",
                 "guidance_scale": 7.5,
                 "use_controlnet": True,
-                "controlnet_path": "canny-xl",
-                "controlnet_type": "canny-xl",
-                "controlnet_strength": 1.0,
+                "controlnet_type": "mistoline+depth-xl",
+                "controlnet_strength": 0.5,
                 "num_inference_steps": 35,
                 "video_path": "./data/example/video_square.mp4",
                 "save_path": "./output/test/XL",
                 "mininterv": 10,
                 "maxinterv": 10,
                 "max_images": 3,
-                "prompt": "A beautiful  photograph of a french street, cinematic, hyperdetailed photograph, shiny scales, 8k resolution,",
+                "prompt": "A beautiful photograph of a french street, cinematic, hyperdetailed photograph, shiny scales, 8k resolution",
                 "negative": "(worst quality, low quality, normal quality, lowres, low details, oversaturated, undersaturated, overexposed, underexposed, grayscale, bw, bad photo, bad photography, bad art:1.4), (watermark, signature, text font, username, error, logo, words, letters, digits, autograph, trademark, name:1.2), (blur, blurry, grainy), morbid, ugly, asymmetrical, mutated malformed, mutilated, poorly lit, bad shadow, draft, cropped, out of frame, cut off, censored, jpeg artifacts, out of focus, glitch, duplicate, (airbrushed, cartoon, anime, semi-realistic, cgi, render, blender, digital art, manga, amateur:1.3), (3D ,3D Game, 3D Game Scene, 3D Character:1.1), (bad hands, bad anatomy, bad body, bad face, bad teeth, bad arms, bad legs, deformities:1.3)",
                 "batch_size": 3,
-                "use_saliency": True,
+                "use_saliency": False,
                 "end_opt_step": 30,
                 "seed": 0,
-                "img_size": 1024,
                 "scheduler": "DDPMScheduler",
                 "ip_adapter_name": "ip-adapter_sdxl.safetensors",
                 "ip_adapter_scale": 0.8,
+                "ip_adapter_style_only": True,
                 "ref_img_ip_adapter": "./data/example/ref_img.png",
-                "strength": 0.85,
-                "noise_type": "integral"
+                "strength": 1.0,
+                "intra_weight": 0.0,
+                "noise_type": "integral",
+                "offload_to_cpu": False
             }
         }
 
@@ -181,7 +148,13 @@ class TclFrescoWrapedNoise:
             config['save_path'] = f"/workspace/ComfyUI/output/{save_path}"
             config['ref_img_ip_adapter'] = ref_img_ip_adapter
             config['noise_type'] = noise_type
+            config['ip_adapter_style_only'] = ip_adapter_style_only
 
+        if model_type == "XL":
+            config['controlnet_type'] = controlnet_type_xl  
+        else:
+            config['controlnet_type'] = controlnet_type_kolors
+   
         # Define the path where the config file will be saved
         config_path = f"/workspace/ComfyUI/input/config_{model_type}.yaml"
 
@@ -194,7 +167,7 @@ class TclFrescoWrapedNoise:
 
         command = [
             'python',
-            '/workspace/FRESCO-wraped-noise/run_fresco_updated.py',
+            '/workspace/FRESCO_17_10_2024/run_fresco_updated.py',
             '--config_path',
             config_path
         ]
@@ -207,7 +180,7 @@ class TclFrescoWrapedNoise:
                 stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT,
                 text=True,
-                cwd="/workspace/FRESCO-wraped-noise",
+                cwd="/workspace/FRESCO_17_10_2024",
                 env=env
             )
             
